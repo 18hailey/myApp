5935e6910bda133e58ff5c01b50e99065e628708
18hailey <hailey9459@gmail.com>
2024-02-22 20:37:03 +0900

    컴포넌트 정리

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import { config } from "@gluestack-ui/config"
 import { GluestackUIProvider, Center, StatusBar, SafeAreaView, ScrollView } from "@gluestack-ui/themed"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Dimensions } from "react-native"
-import MySwiper from "./components/MySwiper"
-import WebtoonList from "./components/WebtoonList"
+import MySwiper from "./components/Header/MySwiper"
+import WebtoonList from "./components/List/WebtoonList"
 
 const queryClient = new QueryClient();
 
--- components/Card/LargeCard.tsx
+++ components/Card/LargeCard.tsx
@@ -32,3 +32,4 @@ export default function LargeCard({ webtoon }: LargeCardProps) {
     </VStack>
   );
 }
+
